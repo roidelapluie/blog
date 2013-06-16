e8c459892e3e9539928fb612ee913233ae3e41f7
Julien Pivotto <roidelapluie@inuits.eu>
2013-06-16 21:58:22 +0200

    first commit

---
+++ content/pages/about-me.md
@@ -0,0 +1,16 @@
+Title: About me
+Date: 2013-06-16
+save_as: index.html
+
+I am a Linux system administrator and open-source consultant at [Inuits](https://inuits.eu), and I believe in DevOps, with focus on automation and monitoring.
+
+# Talks
+
+## 2013
+
+* [Monitoring at Cloud Scale](http://www.slideshare.net/roidelapluie/monitoring-at-cloud-scale) at [Build A Cloud Day Amsterdam 2013](http://bacdamsterdam.eventbrite.com)
+
+## 2012
+
+* [PostgreSQL 9.0 HA](http://www.slideshare.net/roidelapluie/postgresql-90-ha) at [loadays 2012](http://loadays.org)
+* [PostgreSQL 9.0 HA](http://www.slideshare.net/roidelapluie/postgresql-90-ha-at-rmll-2012) at [RMLL 2012](http://20212.rmll.info)
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Julien Pivotto'
+SITENAME = u'sysadmin {\'roidelapluie\':}'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (
+        ('Inuits', 'https://www.inuits.eu/'),
+        ('RMLL', 'https://rmll.info/'),
+        ('Loadays', 'https://loadays.org/'),
+        ('Fosdem', 'https://fosdem.org/'),
+        ('Gajim', 'https://gajim.org/'),
+         )
+# Social widget
+SOCIAL = (
+        ('github', 'https://github.com/roidelapluie'),
+        ('twitter', 'https://twitter.com/roidelapluie'),
+        ('linkedin', 'https://linkedin.com/in/julienpivotto'),
+        ('slideshare', 'http://www.slideshare.net/roidelapluie')
+        )
+
+DEFAULT_PAGINATION = 10
+
+THEME='./notmyidea'
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
