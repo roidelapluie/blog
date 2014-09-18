b18c2ab7374d74328f7bbd69d356a295f88015fd
Julien Pivotto <roidelapluie@inuits.eu>
2014-09-18 22:48:26 +0200

    update gitdiff

--- plugins/gitdiff.py
+++ plugins/gitdiff.py
@@ -14,54 +14,78 @@ ARTICLES = []
 
 _md = Markdown(extensions=MD_EXTENSIONS)
 
-def gengitdiffs(article_generator, writer):
+def workoncontent(git,repo,content):
+    gitcommits = git.log("--format=%H",content.source_path).split("\n")
+    commits = []
+    prevcommit = None
+    prevcommitcontent = None
+    for commithash in reversed(gitcommits):
+        commit = repo.commit(commithash)
+        tree = commit.tree
+        passedcontent = False
+        path = []
+        for doc in content.source_path.split("/"):
+            if doc == "content":
+                passedcontent = True
+            if passedcontent:
+                path.append(doc)
+                tree = tree[doc]
+        commit_content = git.cat_file("-p",tree)
+        if prevcommit == None or prevcommitcontent == None:
+            diff = ["    :::markdown"]
+            for i in commit_content.decode('utf-8').split("\n"):
+                diff.append("     "+ i)
+            print_commit_content = "\n".join(diff)
+        else:
+            fromfile = ["a"] + path
+            tofile = ["b"] + path
+            diff = ["    :::diff"]
+            for i in difflib.unified_diff(prevcommitcontent.split('\n'),commit_content.decode('utf-8').split('\n'),fromfile="/".join(fromfile),tofile="/".join(tofile)):
+                diff.append("    " + i.strip("\n"))
+            print_commit_content = "\n".join(diff)
+
+        commits.append({
+                'hash': commithash,
+                'date': strftime(datetime.fromtimestamp(commit.authored_date), content.date_format),
+                'message': commit.message,
+                'content': _md.convert(print_commit_content)
+            })
+        prevcommit = commithash
+        prevcommitcontent = commit_content.decode('utf-8')
+    return commits
+
+def genarticlegitdiffs(article_generator, writer):
     repo = Repo(".")
     git = repo.git
     for content in article_generator.articles:
-        gitcommits = git.log("--format=%H",content.source_path).split("\n")
-        commits = []
-        prevcommit = None
-        prevcommitcontent = None
-        for commithash in reversed(gitcommits):
-            commit = repo.commit(commithash)
-            tree = commit.tree
-            passedcontent = False
-            path = []
-            for doc in content.source_path.split("/"):
-                if doc == "content":
-                    passedcontent = True
-                if passedcontent:
-                    path.append(doc)
-                    tree = tree[doc]
-            commit_content = git.cat_file("-p",tree)
-            if prevcommit == None or prevcommitcontent == None:
-                diff = ["    :::markdown"]
-                for i in commit_content.decode('utf-8').split("\n"):
-                    diff.append("     "+ i)
-                print_commit_content = "\n".join(diff)
-            else:
-                fromfile = ["a"] + path
-                tofile = ["b"] + path
-                diff = ["    :::diff"]
-                for i in difflib.unified_diff(prevcommitcontent.split('\n'),commit_content.decode('utf-8').split('\n'),fromfile="/".join(fromfile),tofile="/".join(tofile)):
-                    diff.append("    " + i.strip("\n"))
-                print_commit_content = "\n".join(diff)
-
-            commits.append({
-                    'hash': commithash,
-                    'date': strftime(datetime.fromtimestamp(commit.authored_date), content.date_format),
-                    'message': commit.message,
-                    'content': _md.convert(print_commit_content)
-                })
-            prevcommit = commithash
-            prevcommitcontent = commit_content.decode('utf-8')
+        commits = workoncontent(git,repo,content)
 
         writer.write_file("/".join(content.save_as.split('/')[:-1])+"/source.html" , article_generator.get_template('source'),
-              article_generator.context, article=content, source="".join(open(content.source_path,'r').readlines()).decode('utf-8'),
+              article_generator.context, article=content, source="".join(open(content.source_path,'r').readlines()).decode('utf-8'), back_link="index.html",
               override_output=hasattr(content, 'override_save_as'))
         writer.write_file("/".join(content.save_as.split('/')[:-1])+"/changelog.html" , article_generator.get_template('history'),
-              article_generator.context, article=content, gitcommits=commits,
+              article_generator.context, article=content, gitcommits=commits, back_link="index.html",
               override_output=hasattr(content, 'override_save_as'))
 
+def genpagegitdiffs(page_generator, writer):
+    repo = Repo(".")
+    git = repo.git
+    for content in page_generator.pages:
+        commits = workoncontent(git,repo,content)
+
+        writer.write_file(".".join(content.save_as.split('.')[:-1])+"-source.html" , page_generator.get_template('source'),
+              page_generator.context, article=content, source="".join(open(content.source_path,'r').readlines()).decode('utf-8'),
+              override_output=hasattr(content, 'override_save_as'), back_link=content.save_as.split('/')[-1])
+        writer.write_file(".".join(content.save_as.split('.')[:-1])+"-changelog.html" , page_generator.get_template('history'),
+              page_generator.context, article=content, gitcommits=commits,
+              override_output=hasattr(content, 'override_save_as'), back_link=content.save_as.split('/')[-1])
+
+def add_links(content):
+    content.source_link = ".".join(content.save_as.split('/')[-1].split('.')[:-1])+"-source.html"
+    content.change_link = ".".join(content.save_as.split('/')[-1].split('.')[:-1])+"-changelog.html"
+
+
 def register():
-    signals.article_writer_finalized.connect(gengitdiffs)
+    signals.content_object_init.connect(add_links)
+    signals.article_writer_finalized.connect(genarticlegitdiffs)
+    signals.page_writer_finalized.connect(genpagegitdiffs)
