d504b38b974668e1ff1e096bbda8b4b343b46191
Julien Pivotto <roidelapluie@inuits.eu>
2015-08-04 21:46:32 +0200

    Simplify plugin

--- plugins/copyarticle.py
+++ plugins/copyarticle.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python
-
 from pelican import signals
-from pelican.utils import strftime
-
-from datetime import datetime
-import os
-
-
-ARTICLES = []
-
 def gensymlinks(article_generator, writer):
     for content in article_generator.articles:
         if content.source_path == '/home/roidelapluie/blog/blog/content/articles/android-calendar.md':
@@ -18,8 +9,5 @@ def gensymlinks(article_generator, writer):
             writer.write_file(content.save_as.split('/')[-2]+".html" , article_generator.get_template(content.template),
                   article_generator.context, article=content, category=content.category,
                   override_output=hasattr(content, 'override_save_as'))
-        else:
-            print "Skip", content.source_path
-
 def register():
     signals.article_writer_finalized.connect(gensymlinks)
